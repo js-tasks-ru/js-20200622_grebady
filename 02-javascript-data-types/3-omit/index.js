73b50800d77033873ce47f839c10fded29e32fe7
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-07-02 18:17:23 +0300

    1.3 done

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,15 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let newObj = {}
+  let objKeys = Object.keys(obj);
+  for (let i = 0; i < fields.length; i++) {
+    if (objKeys.includes(fields[i])){
+      objKeys.splice(objKeys.indexOf(fields[i]),1);
+    }
+  }
+  for (let i = 0; i < objKeys.length; i++) {
+    newObj[objKeys[i]] = obj[objKeys[i]];
+  }
+  return newObj;
 };
