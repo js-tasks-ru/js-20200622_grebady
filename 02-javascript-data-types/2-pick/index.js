53ac9c8041c6f9b59665c2ba9ace2d36b8c4c988
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-07-02 18:01:50 +0300

    1.2 done

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  let newObj = {}
+  let objKeys = Object.keys(obj);
+  for (let i = 0; i < fields.length; i++) {
+    if (objKeys.includes(fields[i])){
+      newObj[fields[i]] = obj[fields[i]];
+    }
+  }
+  return newObj;
 };
