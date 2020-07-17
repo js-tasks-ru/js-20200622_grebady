c48fd72f4548d36dcc69fae6130533d4310df28f
Gennadiy Grebennik <grebennik2000@yandex.ru>
2020-07-17 14:24:54 +0300

    5.1 done, 5.2 have read and tested

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  element; // HTMLElement;
+  static previousElement;
 
+  constructor(message, {
+    duration = 0,
+    type = ''
+  } = {}) {
+    if (NotificationMessage.previousElement) {
+      NotificationMessage.previousElement.remove();
+    }
+    this.type = type;
+    this.duration = duration;
+    this.message = message;
+    this.render();
+  }
+
+
+  get template() {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="notification ${this.type}" style="--value:${this.duration}ms">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+    `;
+    return div.firstElementChild;
+  }
+
+  render() {
+    this.element = this.template;
+    // this.element.textContent = this.element.textContent.trim();
+  }
+  show(parentElement = document.body) {
+    parentElement.append(this.element);
+    this.idTimer = setTimeout(() => this.remove(), this.duration);
+    NotificationMessage.previousElement = this.element;
+  }
+
+  remove () {
+    this.element.remove();
+    NotificationMessage.previousElement = null;
+  }
+
+  destroy () {
+    this.element.remove();
+    NotificationMessage.previousElement = null;
+  }
 }
